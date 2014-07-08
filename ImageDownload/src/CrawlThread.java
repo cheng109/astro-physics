9ad59edd3f611ae491329213b0db152840127b83
Jun Cheng <cheng109@purdue.edu>
2014-07-08 16:15:10 -0400

    nothing new

--- ImageDownload/src/CrawlThread.java
+++ ImageDownload/src/CrawlThread.java
@@ -1,23 +1,22 @@
 package ImageDownload;
 
 class CrawlThread implements Runnable {
-	Thread t;
-	String task = new String(); 
-	WebCrawl WebCrawler; 
-	CrawlThread(String task) {
-		this.task=task; 
-		WebCrawler = new WebCrawl(task); 
-		t = new Thread(this, "crawling"); 
-		t.start(); 
-	}
-	public void run() {
-		WebCrawler.startCrawl();
-		WebCrawler.crawl(); 
-	}
+    Thread t;
+    String task = new String(); 
+    WebCrawl WebCrawler; 
+    CrawlThread(String task) {
+	this.task=task; 
+	WebCrawler = new WebCrawl(task); 
+	t = new Thread(this, "crawling"); 
+	t.start(); 
+    }
+    public void run() {
+	WebCrawler.startCrawl();
+	WebCrawler.crawl(); 
+    }
 	
-	public static void main (String[] args) {
-	final CrawlThread crawlThread1 = new CrawlThread("task1"); 
-			
-	}
+    public static void main (String[] args) {
+	final CrawlThread thread = new CrawlThread("task1"); 
+    }
 		
 }
--- ImageDownload/src/WebCrawl.java
+++ ImageDownload/src/WebCrawl.java
@@ -1,98 +1,77 @@
 package ImageDownload; 
-
-
 import java.io.File;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
 
-
 public class WebCrawl {
+    public Crawler crawler = new Crawler();
+    public int maxurls; 
+    public Properties props;  
+    public String root; 
+    public String task; 
+    public WebCrawl(String task){
+	this.task=task;
+    }
 	
-	public Crawler crawler = new Crawler();
-	public int maxurls; 
-	public Properties props;  
-	public String root; 
-	public String task; 
-	public WebCrawl(String task){
-		this.task=task;
+    public void startCrawl() {
+	crawler.NextURLIDScanned = 0; 
+	String folder=new String(); 
+	try {
+	    crawler.readProperties();
+	    String parentPath=crawler.props.getProperty("download.path"); 
+	    root = crawler.props.getProperty("crawler.root");
+	    folder = crawler.props.getProperty("folder"); 
+	    crawler.jdbcURL = crawler.props.getProperty("jdbc.url");
+	    
+	    int pos = crawler.jdbcURL.lastIndexOf('/'); 
+	    crawler.nameDB = crawler.jdbcURL.substring(pos+1, crawler.jdbcURL.length()); 
+	    System.out.println("DB Name:" + crawler.nameDB +"\n================="); 
+	    crawler.path = parentPath + folder+ "/"; 
+	    File f=new File(crawler.path); 
+	    if(!f.isDirectory()) {
+		f.mkdir(); 
+	    }
+	    maxurls = Integer.parseInt(crawler.props.getProperty("crawler.maxurls")); 
+	    crawler.createDB();
+	    crawler.insertURLInDB(root); 
+	    crawler.ImageurlID = 0; 
+	    crawler.urlID=1;
+	    crawler.fetchURL(root);	
+	    crawler.fetchDescription(root, crawler.NextURLIDScanned); 
+	    crawler.fetchImageURL(root); 
+	}catch(Exception e) {
+	    e.printStackTrace();
+	    System.out.println("haha2\n");
 	}
+    }
 	
-	public void startCrawl() {
-		crawler.NextURLIDScanned = 0; 
-		String folder=new String(); 
-		try {
-			crawler.readProperties();
-			String parentPath=crawler.props.getProperty("download.path"); 
-		
-			if(task=="task1") {
-				root = crawler.props.getProperty("crawler.root1");
-				folder = crawler.props.getProperty("folder1"); 
-				crawler.jdbcURL = crawler.props.getProperty("jdbc.url1");
-			}
-			if(task=="task2") {
-				root = crawler.props.getProperty("crawler.root2");
-				folder = crawler.props.getProperty("folder2"); 
-				crawler.jdbcURL = crawler.props.getProperty("jdbc.url2");
-			}
+    public void crawl()  {
+	while(crawler.NextURLIDScanned<crawler.NextURLID) {	
+	    try {
+		Statement statFetchURL = crawler.connection.createStatement();
+		String query = "select url from URLS where (urlid ='"+crawler.NextURLIDScanned+"' )" ;
+		statFetchURL.executeQuery( query );
+		ResultSet rs = statFetchURL.getResultSet ();
 			
-			int pos = crawler.jdbcURL.lastIndexOf('/'); 
-			crawler.nameDB = crawler.jdbcURL.substring(pos+1, crawler.jdbcURL.length()); 
-			System.out.println(crawler.nameDB); 
-			crawler.path = parentPath + folder+ "/"; 
-			File f=new File(crawler.path); 
-			if(!f.isDirectory()) {
-				f.mkdir(); 
-			}
-			maxurls = Integer.parseInt(crawler.props.getProperty("crawler.maxurls")); 
-			System.out.println(crawler.jdbcURL); 
-			crawler.createDB();
-			crawler.insertURLInDB(root); 
-			crawler.ImageurlID = 0; 
-			crawler.urlID=1;
-			crawler.fetchURL(root);	
-			crawler.fetchDescription(root, crawler.NextURLIDScanned); 
-
-			crawler.fetchImageURL(root); 
-		}
-		catch( Exception e) {
-	         e.printStackTrace();
-		}
-	}
-	
-	public void crawl()  {
-		while(crawler.NextURLIDScanned<crawler.NextURLID) {	
-			try {
-				Statement statFetchURL = crawler.connection.createStatement();
-				String query = "select url from URLS where (urlid ='"+crawler.NextURLIDScanned+"' )" ;
-				statFetchURL.executeQuery( query );
-				ResultSet rs = statFetchURL.getResultSet ();
-			
-				rs.next(); 
-				String url1 = rs.getString("url"); 
-				url1 = url1.replace('\"','\0');
-				if(crawler.urlID<maxurls) {
-					crawler.fetchURL(url1);
-					System.out.println(crawler.NextURLIDScanned+ "/" + maxurls); 
-					System.out.println("[urlID]=" +crawler.urlID); 
-
-				}	
-				crawler.fetchDescription(url1, crawler.NextURLIDScanned); 
-				crawler.fetchImageURL(url1); 
-			}
-			
-
-			catch( Exception e) {
-		         e.printStackTrace();
-			}
-			crawler.NextURLIDScanned++; 
-		}
-		System.out.println("nexturlid = "+crawler.NextURLID);     		
-
+		rs.next(); 
+		String url1 = rs.getString("url"); 
+		url1 = url1.replace('\"','\0');
+		if(crawler.urlID<maxurls) {
+		    crawler.fetchURL(url1);
+		    System.out.println(crawler.NextURLIDScanned+ "/" + maxurls); 
+		    System.out.println("[urlID]=" +crawler.urlID); 
 		}	
+		crawler.fetchDescription(url1, crawler.NextURLIDScanned); 
+		crawler.fetchImageURL(url1); 
+	    }
+	    catch( Exception e) {
+		e.printStackTrace();
+		//		continue; 
+	    }
+	    crawler.NextURLIDScanned++; 
+	}
+	System.out.println("nexturlid = "+crawler.NextURLID);     		
 
-	
-	
-	
-	
+    }	
 }
